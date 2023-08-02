44ff83afb7bd54d314ec3cc16522bb0348792bfd
tanujsonare <tanujsonare1910@gmail.com>
2023-08-02 14:33:29 +0530

    Updated asgi file for websockets

--- chat_application/asgi.py
+++ chat_application/asgi.py
@@ -10,7 +10,23 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+
+from chat import routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chat_application.settings")
 
-application = get_asgi_application()
+django_asgi_application = get_asgi_application()
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_application,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(
+                URLRouter(routing.websocket_urlpatterns)
+            )
+        )
+    }
+)
