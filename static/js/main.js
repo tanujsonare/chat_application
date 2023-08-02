c4facb272866806175e6254df16359b4f24df052
tanujsonare <tanujsonare1910@gmail.com>
2023-08-02 16:48:26 +0530

    Added changes for tailwind css and show chat bubble

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,11 @@
+const chatElement = document.querySelector("#chat");
+const chatOpen = document.querySelector("#chat_open");
+const joinChat = document.querySelector("#join_chat");
+const chatWelcome = document.querySelector("#chat_welcome");
+const chatIcon = document.querySelector("#chat_icon");
 
+chatOpen.onclick = function(e){
+    e.preventDefault();
+    chatIcon.classList.add("hidden");
+    chatWelcome.classList.remove('hidden');
+}
