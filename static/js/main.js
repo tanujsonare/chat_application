4ce94423bf5bea407daa5772e49ea3c66ae516a4
tanujsonare <tanujsonare1910@gmail.com>
2023-08-02 17:47:51 +0530

    Fixed chat bubble welcome form issue

--- static/js/main.js
+++ static/js/main.js
@@ -5,7 +5,16 @@ const chatWelcome = document.querySelector("#chat_welcome");
 const chatIcon = document.querySelector("#chat_icon");
 
 chatOpen.onclick = function(e){
-    e.preventDefault();
     chatIcon.classList.add("hidden");
     chatWelcome.classList.remove('hidden');
+    e.stopPropagation();   // Prevent click event from propagating to the body
 }
+
+document.addEventListener('click', function(event) {
+    if (!chatWelcome.classList.contains("hidden")){
+        if (!chatWelcome.contains(event.target)) {
+            chatIcon.classList.remove("hidden");
+            chatWelcome.classList.add('hidden');
+        }
+    }
+});
