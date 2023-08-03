8c570a013ac5082b13b71813c226f7b031a5caaf
tanujsonare <tanujsonare1910@gmail.com>
2023-08-03 15:01:00 +0530

    Call create chat room api on home page

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,32 @@
+let windowUrl = window.location.href;
+let chatName = '';
+let chatRoomUuid = Math.random().toString(36).slice(2, 12);
+
 const chatElement = document.querySelector("#chat");
 const chatOpen = document.querySelector("#chat_open");
 const joinChat = document.querySelector("#join_chat");
 const chatWelcome = document.querySelector("#chat_welcome");
 const chatIcon = document.querySelector("#chat_icon");
+const chatNameElement = document.querySelector("#user_name")
+
+
+// csrf token 
+
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
 
 chatOpen.onclick = function(e){
     chatIcon.classList.add("hidden");
@@ -18,3 +42,47 @@ document.addEventListener('click', function(event) {
         }
     }
 });
+
+function joinChatRoom(){
+    const data = new FormData();
+    chatName = chatNameElement.value;
+    if(chatName){
+        data.append("name", chatName);
+    }
+    if(windowUrl){
+        data.append("url", windowUrl);
+    }
+
+    var settings = {
+        url: `api/create_room/${chatRoomUuid}/`,
+        method: "POST",
+        timeout: 2000,
+        retryCount: 0,
+        retryLimit: 4,
+        processData: false,
+        mimeType: "multipart/form-data",
+        contentType: false,
+        dataType: "json",
+        headers:{"X-CSRFToken": Cookies.get('csrftoken')},
+        data: data,
+    };
+
+    $.ajax(settings).done(function (response) {
+        if (response && response.message == "Room created successfully"){
+            window.location.href = "/chat";
+        }
+    }).fail(function (errorThrown) {
+    // is called if request fails or timeout is reached
+    settings.retryCount++;
+    if (settings.retryCount <= settings.retryLimit && Date.now() - settings.created < settings.retryTimeout) {
+        console.log("Retrying");
+        $.ajax(settings);
+        return;
+    }
+    });
+}
+
+joinChat.addEventListener('click', function(e) {
+    e.preventDefault();
+    joinChatRoom();
+})
