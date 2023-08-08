33f5b9e01605fa8626e72199681f996489bbaebd
tanujsonare <tanujsonare1910@gmail.com>
2023-08-08 13:15:02 +0530

    created an admin dashboard

--- chat/urls.py
+++ chat/urls.py
@@ -7,4 +7,5 @@ app_name = "chat"
 urlpatterns = [
     path("api/create_room/<str:uuid>/", views.create_room, name="create_room"),
     path("chat", views.chat, name="create_room"),
+    path("admin-dashboard", views.admin_dashboard, name="admin_dashboard"),
 ]
--- chat/views.py
+++ chat/views.py
@@ -2,9 +2,10 @@ import json
 from django.shortcuts import render
 from django.http.response import JsonResponse
 from django.views.decorators.http import require_POST
+from django.contrib.auth.decorators import login_required
 
 from .models import Room
-
+from account.models import User
 
 @require_POST
 def create_room(request, uuid):
@@ -20,3 +21,13 @@ def create_room(request, uuid):
 
 def chat(request):
     return render(request, "chat/chat_page_for_customer.html")
+
+@login_required
+def admin_dashboard(request):
+    rooms = Room.objects.all()
+    staff_users = User.objects.filter(is_staff=True)
+
+    return render(request, "chat/admin_dashboard.html",{
+        "rooms": rooms,
+        "staff_users": staff_users
+    })
