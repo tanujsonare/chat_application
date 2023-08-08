7ccf8be5927c42bf5dc18ec369d01dbede718f5a
tanujsonare <tanujsonare1910@gmail.com>
2023-08-08 13:18:59 +0530

    Updated chat bubble UI

--- chat/template_tages/chat_tags.py
+++ chat/template_tages/chat_tags.py
@@ -5,9 +5,8 @@ register = template.Library()
 @register.filter(name="initials")
 def initials(value):
     initials = ""
-    # breakpoint()
     for name in value.split(" "):
-        if name and len(name) < 3:
+        if name:
             initials += name[0].upper()
 
     return initials
