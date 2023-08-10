2022e475dc84ab4f8fb2758b1f06f316ad23397e
tanujsonare <tanujsonare1910@gmail.com>
2023-08-10 12:41:01 +0530

    Connect websocket for admin user

--- static/js/chat_admin.js
+++ static/js/chat_admin.js
@@ -1,11 +1,27 @@
 const chatRoomUuid = document.querySelector("#room_uuid").textContent.replaceAll('"', '');
-var chatSocket;
+var chatWebSocket;
 
 const chatText = document.querySelector("#message_content");
 const sendMessageButton = document.querySelector("#send_message");
 const messageBox = document.querySelector("#message_box");
 
-chatWebSocket = new WebSocket(`ws://${window.location.host}/ws/${chatRoomUuid}/`);
+$(document).ready(function () {
+    if (chatRoomUuid){
+        chatWebSocket = new WebSocket(`ws://${window.location.host}/ws/${chatRoomUuid}/`)
+        chatWebSocket.onopen = function(e){
+            console.log("chat socket open");
+        }
+        chatWebSocket.onclose = function(e){
+            console.log("chat socket close");
+        }
+        if(chatWebSocket){
+            chatWebSocket.onmessage = function(e){
+                console.log("On message calls");
+                onNewMessage(JSON.parse(e.data));
+            };
+        }
+    }
+})
 
 function sendMesage(){
     if (chatWebSocket){
@@ -21,35 +37,23 @@ function sendMesage(){
 
 function onNewMessage(data){
     if(data.type == "chat_message" && data.message && !data.agent){
-         messageBox.innerHTML += `
-         <div class="col-start-1 col-end-8 p-3 rounded-lg">
-             <div class="flex flex-row items-center">
-                 <div
-                     class="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
-                     ${data.initials}
-                 </div>
-                 <div class="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl">
-                     <div>
-                         ${data.message}
-                     </div>
-                     <span class="text-xs text-gray-500 leading-none">${data.created_at} ago</span>
-                 </div>
-             </div>
-         </div>
-         `
+        let new_message = '';
+        new_message += `
+            <div class="flex w-full max-w-md mt-2 space-x-3 ${data.agent ? "ml-auto justify-end" : ""}">
+                <div class="flex-shrink-0 h-10 w-10 rounded-full bg-indigo-500 text-center text-white mx-3 my-2 pt-2">
+                    ${data.initials}
+                </div>
+            `
+        new_message += `
+                <div class="my-2">
+                    <div class="p-3 rounded-xl ${data.agent ? "bg-gray-600": "bg-white"}">
+                        <p class="text-sm">${data.message}</p>
+                    </div>
+                    <span class="text-xs text-gray-700 leading-none">${data.created_at} ago</span>
+                </div>
+            </div>
+        `
+        $("#message_box").append(new_message);
+        messageBox.scrollTo(0, messageBox.scrollHeight);
     }
- }
-
-if (chatSocket){
-    chatSocket.onmessage = function(e){
-        onNewMessage(JSON.parse(e.data));
-    }
-}
-
-chatSocket.onopen = function(e){
-
-}
-
-chatSocket.onclose = function(e){
-
 }
