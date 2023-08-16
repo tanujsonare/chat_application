8261e1a957dc850057059ef6c30054d0fba9bac9
tanujsonare <tanujsonare1910@gmail.com>
2023-08-16 14:38:12 +0530

    Added UI for register page

--- account/forms.py
+++ account/forms.py
@@ -8,9 +8,17 @@ class LoginForm(AuthenticationForm):
     class Meta:
         model = User
         fields = ('username', 'password',)
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': 'w-full py-2 my-3 px-6 rounded-xl border'
+            }),
+            'password': forms.PasswordInput(attrs={
+                'class': 'w-full py-2 my-3 px-6 rounded-xl border'
+            })
+        }
 
 
-class AddUserForm(forms.ModelForm):
+class AddAdminUserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('email', 'name', 'role', 'password',)
@@ -24,10 +32,38 @@ class AddUserForm(forms.ModelForm):
             'role': forms.Select(attrs={
                 'class': 'w-full py-2 my-3 px-6 rounded-xl border'
             }),
-            'password': forms.TextInput(attrs={
+            'password': forms.PasswordInput(attrs={
                 'class': 'w-full py-2 my-3 px-6 rounded-xl border'
             })
         }
+
+
+class AddUserForm(forms.ModelForm):
+    retype_password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'w-full py-2 my-3 px-6 rounded-xl border'}))
+    class Meta:
+        model = User
+        fields = ('email', 'name', 'password')
+        widgets = {
+            'email': forms.TextInput(attrs={
+                'class': 'w-full py-2 my-3 px-6 rounded-xl border'
+            }),
+            'name': forms.TextInput(attrs={
+                'class': 'w-full py-2 my-3 px-6 rounded-xl border'
+            }),
+            'password': forms.PasswordInput(attrs={
+                'class': 'w-full py-2 my-3 px-6 rounded-xl border'
+            })
+        }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        retype_password = cleaned_data.get('retype_password')
+
+        if password and retype_password and password != retype_password:
+            self.add_error('retype_password', "Passwords does not match !!")
+
+        return cleaned_data
 
 
 class EditUserForm(forms.ModelForm):
