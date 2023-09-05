390b9df0eb1fdd5bddb476e8aeb5b6a1d1c2cba5
tanujsonare <tanujsonare1910@gmail.com>
2023-09-05 15:53:21 +0530

    Added yaml file for CI

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
 # Create your tests here.
+
+
+class UserTestCase(TestCase):
+    def test_user(self):
+        name = 'Tanuj'
+        password = 'hello'
+        u = User(name=name)
+        u.set_password(password)
+        u.save()
+        self.assertEqual(u.name, name)
+        self.assertTrue(u.check_password(password))
