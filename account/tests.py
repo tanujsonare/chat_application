07d450965ca4a4d6da3e08ed7ba827885651cb1d
tanujsonare <tanujsonare1910@gmail.com>
2023-09-05 18:31:21 +0530

    Fixed test cases and updated requirements.txt file

--- account/tests.py
+++ account/tests.py
@@ -1,12 +1,12 @@
 from django.test import TestCase
-from django.contrib.auth.models import User
+from account.models import User
 
 # Create your tests here.
 
 
 class UserTestCase(TestCase):
     def test_user(self):
-        name = 'Tanuj'
+        name = 'Test case user'
         password = 'hello'
         u = User(name=name)
         u.set_password(password)
