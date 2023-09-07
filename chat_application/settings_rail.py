685499ec8b2ae34798293faf587f16bc1f073b2d
tanujsonare <tanujsonare1910@gmail.com>
2023-09-07 19:15:14 +0530

    Final

--- chat_application/settings_rail.py
+++ chat_application/settings_rail.py
@@ -2,6 +2,6 @@ from .settings import *
 from os import environ
 
 
-SECRET_KEY = environ.get('SECRET_KEY')
+SECRET_KEY = "django-insecure-%@l^ihlsu)xn8bagu@dmaatua+odxs19=6uy(l=@r)5m!i4ov@"
 
 ALLOWED_HOSTS = ["chatapplication-production-e945.up.railway.app", "localhost", "*"]
